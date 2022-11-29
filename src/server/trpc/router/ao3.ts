420fd740b45fbd72a16320794f133c86b152dfcf
JonathanG321 <JonathanG321@ymail.com>
2022-11-29 12:52:14 -0500

    finish a03 data retrieval

--- src/server/trpc/router/ao3.ts
+++ src/server/trpc/router/ao3.ts
@@ -1,80 +1,201 @@
 import { z } from "zod";
+import { parse } from "node-html-parser";
 
 import { router, publicProcedure } from "../trpc";
 
-function ifTuple(tuple: Array<number | null>) {
+import type { HTMLElement } from "node-html-parser";
+
+type Work = {
+  author: string;
+  bookmarks?: number;
+  link: string;
+  categories: string[];
+  chapterCount: number;
+  characters: string[];
+  complete: boolean;
+  comments?: number;
+  expectedChapters: number;
+  fandoms: string[];
+  hits: number;
+  kudos: number;
+  language?: string;
+  rating?: string;
+  relationships: string[];
+  restricted: boolean;
+  summary: string;
+  tags: string[];
+  title: string;
+  warnings: string[];
+  id: number;
+  words: number;
+  collections?: number;
+  dateUpdated: Date;
+  datePublished: Date;
+};
+
+function ifTuple(tuple?: Array<number | undefined>) {
+  if (!tuple) return;
   return tuple[1] ? `${tuple[0]}-${tuple[1]}` : `${tuple[0]}`;
 }
 
-function urlMerge(url: string, field: string, data: string) {
-  return url.concat(`work_search[${field}]=${data}`);
+function urlMerge(url: string, field: string, data?: string) {
+  return url.concat(`&work_search[${field}]=${data || ""}`);
+}
+
+function getTextFromHTML(html: HTMLElement, querySelector: string) {
+  return html.querySelector(querySelector)?.innerText;
+}
+function getNumberFromHTML(html: HTMLElement, querySelector: string) {
+  return parseInt(getTextFromHTML(html, querySelector) || "0", 10);
+}
+
+function AO3Input() {
+  return z
+    .object({
+      anyField: z.string().optional().default(""),
+      title: z.string().optional().default(""),
+      author: z.string().optional().default(""),
+      singleChapter: z.boolean().optional().default(false),
+      wordCount: z.tuple([z.number(), z.number().optional()]).optional(),
+      language: z.string().optional().default(""),
+      fandoms: z.string().optional().default(""),
+      rating: z.tuple([z.number(), z.number().optional()]).optional(),
+      hits: z.tuple([z.number(), z.number().optional()]).optional(),
+      kudos: z.tuple([z.number(), z.number().optional()]).optional(),
+      crossovers: z.boolean().optional(),
+      bookmarks: z.tuple([z.number(), z.number().optional()]).optional(),
+      comments: z.tuple([z.number(), z.number().optional()]).optional(),
+      completionStatus: z.boolean().optional(),
+      page: z.number().optional().default(1),
+      sortColumn: z.string().optional().default(""),
+      sortDirection: z.string().optional().default(""),
+      revisedAt: z.string().optional().default(""),
+      session: z.null().optional().default(null),
+      characters: z.string().optional().default(""),
+      relationships: z.string().optional().default(""),
+      tags: z.string().optional().default(""),
+    })
+    .nullish();
+}
+
+const AO3InputType = AO3Input()._type;
+
+function getAO3URL(input: NonNullable<typeof AO3InputType>) {
+  let url = "https://archiveofourown.org/works/search?";
+  url = urlMerge(url, "query", input.anyField != "" ? input.anyField : " ");
+  url = url.concat(`&page=${input.page}`);
+  url = urlMerge(url, "title", input.title);
+  url = urlMerge(url, "creators", input.author);
+  if (input.singleChapter) url = url.concat(`&work_search[single_chapter]=1`);
+  url = urlMerge(url, "word_count", ifTuple(input.wordCount));
+  url = urlMerge(url, "language_id", input.language);
+  url = urlMerge(url, "fandom_names", input.fandoms);
+  url = urlMerge(url, "character_names", input.characters);
+  url = urlMerge(url, "relationship_names", input.relationships);
+  url = urlMerge(url, "freeform_names", input.tags);
+  url = urlMerge(url, "rating_ids", ifTuple(input.rating));
+  url = urlMerge(url, "hits", ifTuple(input.hits));
+  url = urlMerge(url, "kudos_count", ifTuple(input.kudos));
+  if (input.crossovers !== undefined) url = urlMerge(url, "crossover", input.crossovers ? "T" : "F");
+  url = urlMerge(url, "bookmarks_count", ifTuple(input.bookmarks));
+  url = urlMerge(url, "comments_count", ifTuple(input.comments));
+  if (input.completionStatus !== undefined)
+    url = urlMerge(url, "complete", input.completionStatus ? "T" : "F");
+  url = urlMerge(url, "sort_column", input.sortColumn);
+  url = urlMerge(url, "sort_direction", input.sortDirection);
+  url = urlMerge(url, "revised_at", input.revisedAt);
+  return url;
+}
+
+async function getWorkFromArticle(article: HTMLElement): Promise<Work> {
+  const chapters = article.querySelector("dd.chapters")?.innerText.split("/");
+  const id = parseInt(
+    article.querySelector("h4.heading a")?.attrs["href"]?.replace("/works/", "") || "0",
+    10
+  );
+  const work = parse(
+    await (
+      await fetch(`https://archiveofourown.org/works/${id}`, {
+        headers: {
+          cookie: "accepted_tos=20180523; view_adult=true;",
+        },
+      })
+    ).text()
+  );
+  const datePublished = new Date(work.querySelector("dl.stats dd.published")?.innerText || "");
+  const categoryString = getTextFromHTML(article, "dd.categories");
+  return {
+    title: article.querySelectorAll("h4.heading a")[0]?.innerText || "",
+    link: `https://archiveofourown.org/works/${id}`,
+    author: article.querySelectorAll("h4.heading a")[1]?.innerText || "",
+    id,
+    fandoms: article.querySelectorAll("h5.fandoms.heading a.tag").map((fandom) => fandom.innerText),
+    warnings: article.querySelectorAll("ul.tags li.warnings a.tag").map((warning) => warning.innerText),
+    relationships: article
+      .querySelectorAll("ul.tags li.relationships a.tag")
+      .map((warning) => warning.innerText),
+    characters: article.querySelectorAll("ul.tags li.characters a.tag").map((warning) => warning.innerText),
+    tags: article.querySelectorAll("ul.tags li.freeforms a.tag").map((warning) => warning.innerText),
+    summary: article
+      .querySelectorAll("blockquote.summary p")
+      .map((warning) => warning.innerText)
+      .join("/n"),
+    words: getNumberFromHTML(article, "dd.words"),
+    language: article.querySelector("dd.language")?.innerText,
+    chapterCount: parseInt(chapters && chapters[0] ? chapters[0] : "" || "0", 10),
+    expectedChapters: parseInt(chapters && chapters[1] ? chapters[1] : "0", 10),
+    hits: getNumberFromHTML(article, "dd.hits"),
+    complete: chapters ? chapters[0] === chapters[1] : false,
+    comments: getNumberFromHTML(article, "dd.comments"),
+    collections: getNumberFromHTML(article, "dd.collections"),
+    bookmarks: getNumberFromHTML(article, "dd.bookmarks"),
+    kudos: getNumberFromHTML(article, "dd.kudos"),
+    restricted: !!article.querySelector("img[title=restricted]"), // TODO: test this
+    dateUpdated: new Date(article.querySelector("p.datetime")?.innerText || ""), // TODO: test this
+    categories: !!categoryString ? categoryString.split(", ") : [],
+    rating: getTextFromHTML(article, "span.rating"),
+    datePublished,
+  };
+}
+
+async function processAO3Search(req: Response) {
+  const parsed = parse(await req.text());
+  const results = parsed.querySelector("ol.work.index.group");
+
+  if (
+    !results ||
+    !!parsed
+      .querySelector("#main.works-search.region")
+      ?.childNodes.find(
+        (node) =>
+          node.textContent === "No results found. You may want to edit your search to make it less specific."
+      )
+  ) {
+    return;
+  }
+
+  const articles = results.querySelectorAll("li[role='article']");
+
+  const works: Work[] = await Promise.all(articles.map(async (article) => await getWorkFromArticle(article)));
+
+  return works;
 }
 
 export const AO3Router = router({
-  search: publicProcedure
-    .input(
-      z
-        .object({
-          anyField: z.string().default(""),
-          title: z.string().default(""),
-          author: z.string().default(""),
-          singleChapter: z.boolean().default(false),
-          wordCount: z.tuple([z.number(), z.number()]).nullable(),
-          language: z.string().default(""),
-          fandoms: z.string().default(""),
-          rating: z.tuple([z.number(), z.number().nullable().default(null)]).nullable(),
-          hits: z.tuple([z.number(), z.number().nullable().default(null)]).nullable(),
-          kudos: z.tuple([z.number(), z.number().nullable().default(null)]).nullable(),
-          crossovers: z.boolean().default(false),
-          bookmarks: z.tuple([z.number(), z.number().nullable().default(null)]).nullable(),
-          comments: z.tuple([z.number(), z.number().nullable().default(null)]).nullable(),
-          completionStatus: z.boolean().nullable(),
-          page: z.number().default(1),
-          sortColumn: z.string().default(""),
-          sortDirection: z.string().default(""),
-          revisedAt: z.string().default(""),
-          session: z.null().default(null),
-          characters: z.string().default(""),
-          relationships: z.string().default(""),
-          tags: z.string().default(""),
-        })
-        .nullish()
-    )
-    .query(async ({ input, ctx }) => {
-      if (!input) throw new Error("Something went wrong with the AO3 Router. No input was given.");
-      const url = "https://archiveofourown.org/works/search?";
-      urlMerge(url, "query", input.anyField != "" ? input.anyField : " ");
-      if (input.page !== 1) url.concat(`page=${input.page}`);
-      if (input.title !== "") urlMerge(url, "title", input.title);
-      if (input.author !== "") urlMerge(url, "creators", input.author);
-      if (input.singleChapter) url.concat(`work_search[single_chapter]=1`);
-      if (input.wordCount !== null) urlMerge(url, "word_count", ifTuple(input.wordCount));
-      if (input.language !== "") urlMerge(url, "language_id", input.language);
-      if (input.fandoms !== "") urlMerge(url, "fandom_names", input.fandoms);
-      if (input.characters !== "") urlMerge(url, "character_names", input.characters);
-      if (input.relationships !== "") urlMerge(url, "relationship_names", input.relationships);
-      if (input.tags !== "") urlMerge(url, "freeform_names", input.tags);
-      if (input.rating !== null) urlMerge(url, "rating_ids", ifTuple(input.rating));
-      if (input.hits !== null) urlMerge(url, "hits", ifTuple(input.hits));
-      if (input.kudos !== null) urlMerge(url, "kudos_count", ifTuple(input.kudos));
-      if (input.crossovers !== null) urlMerge(url, "crossover", input.crossovers ? "T" : "F");
-      if (input.bookmarks !== null) urlMerge(url, "bookmarks_count", ifTuple(input.bookmarks));
-      if (input.comments !== null) urlMerge(url, "comments_count", ifTuple(input.comments));
-      if (input.completionStatus !== null) urlMerge(url, "complete", input.completionStatus ? "T" : "F");
-      if (input.sortColumn !== "") urlMerge(url, "sort_column", input.sortColumn);
-      if (input.sortDirection !== "") urlMerge(url, "sort_direction", input.sortDirection);
-      if (input.revisedAt !== "") urlMerge(url, "revised_at", input.revisedAt);
+  search: publicProcedure.input(AO3Input()).query(async ({ input }) => {
+    if (!input) throw new Error("Something went wrong with the AO3 Router. No input was given.");
 
-      let req = null;
-      if (input.session === null) req = await fetch(url);
-      // else req = session.get(url)
-      if (!req) throw new Error("Something went wrong with fetching from AO3. No request was returned.");
-      if (req.status === 429)
-        throw new Error("We are being rate-limited. Try again in a while or reduce the number of requests");
-      // const soup = BeautifulSoup(req.content, features="lxml")
-      // return soup
-    }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+    const url = getAO3URL(input);
+
+    // const search = { results: [], totalResults: 0, pages: 0 };
+
+    let req = null;
+    if (input.session === null) req = await fetch(url);
+    // else req = session.get(url)
+    if (!req) throw new Error("Something went wrong with fetching from AO3. No request was returned.");
+    if (req.status === 429)
+      throw new Error("We are being rate-limited. Try again in a while or reduce the number of requests");
+
+    return processAO3Search(req);
   }),
 });
