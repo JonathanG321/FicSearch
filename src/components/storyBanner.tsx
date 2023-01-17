f4632e6a569d57bed786e0863ee8aa7643fd7b63
JonathanG321 <JonathanG321@ymail.com>
2023-01-16 21:24:18 -0500

    add styling to storyBanner

--- src/components/storyBanner.tsx
+++ src/components/storyBanner.tsx
@@ -44,77 +44,77 @@ function StoryBanner({
 }: StoryBannerType) {
   const completeText = complete === true ? "Complete" : complete === false ? "Incomplete" : undefined;
   return (
-    <div className="m-4 rounded-md border">
-      <div className="flex justify-between">
+    <div className="m-4 rounded-md border p-4 text-white">
+      <div className="mb-1 flex justify-between">
         <div className="flex">
           <Link href={link}>
-            <h3 className="font-bold">{title}</h3>
+            <h3 className="mr-1 font-bold">{title}</h3>
           </Link>
           <h3 className="text-neutral-500">by: {author}</h3>
         </div>
         <h3>{origin}</h3>
       </div>
-      <div className="flex">
+      <div className="my-2 flex">
         {fandoms && fandoms[0] && (
           <>
             <p className="mr-2">Fandoms:</p>
             {fandoms.map((fandom) => (
-              <p key={fandom} className="mr-2 bg-cyan-300">
+              <p key={fandom} className="mr-2 rounded bg-cyan-300 px-2">
                 {fandom}
               </p>
             ))}
           </>
         )}
       </div>
-      <div className="flex">
+      <div className="my-2 flex">
         {tags && tags[0] && (
           <>
             <p className="mr-2">Tags:</p>
             {tags.map((tag) => (
-              <p key={tag} className="mr-2 bg-cyan-300">
+              <p key={tag} className="mr-2 rounded bg-cyan-300 px-2">
                 {tag}
               </p>
             ))}
           </>
         )}
       </div>
-      <div className="flex">
+      <div className="my-2 flex">
         {characters && characters[0] && (
           <>
             <p className="mr-2">Characters:</p>
             {characters.map((character) => (
-              <p key={character} className="mr-2 bg-cyan-300">
+              <p key={character} className="mr-2 rounded bg-cyan-300 px-2">
                 {character}
               </p>
             ))}
           </>
         )}
       </div>
-      <div className="flex">
+      <div className="my-2 flex">
         {relationships && relationships[0] && (
           <>
             <p className="mr-2">Relationships:</p>
             {relationships.map((relationship) => (
-              <p key={relationship} className="mr-2 bg-cyan-300">
+              <p key={relationship} className="mr-2 rounded bg-cyan-300 px-2">
                 {relationship}
               </p>
             ))}
           </>
         )}
       </div>
-      <div className="flex">
+      <div className="my-2 flex">
         {warnings && warnings[0] && (
           <>
             <p className="mr-2">Warnings:</p>
             {warnings.map((warning) => (
-              <p key={warning} className="mr-2 bg-cyan-300">
+              <p key={warning} className="mr-2 rounded bg-cyan-300 px-2">
                 {warning}
               </p>
             ))}
           </>
         )}
       </div>
-      <div className="flex">
+      <div className="my-2 flex">
         {completeText && <p>{completeText}</p>}
         <p>Words: {words}</p>
         <p>Chapters: {chapterCount}</p>
