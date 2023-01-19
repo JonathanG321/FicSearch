c94255374ec7286c6e28f2672ba20d0dd761afff
JonathanG321 <JonathanG321@ymail.com>
2023-01-18 21:54:49 -0500

    add AO3Search type

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -14,6 +14,29 @@ type SpacebattlesSearch = {
   order?: "date" | "relevance" | "words" | "last_update" | "replies";
 };
 
+type AO3Search = {
+  anyField: string;
+  title: string;
+  author: string;
+  singleChapter: boolean;
+  wordCount: number[];
+  language: string;
+  fandoms: string[];
+  rating: number[];
+  hits: number[];
+  kudos: number[];
+  crossovers: boolean;
+  bookmarks: number[];
+  comments: number[];
+  completionStatus: boolean;
+  sortColumn: string;
+  sortDirection: string;
+  revisedAt: string;
+  characters: string;
+  relationships: string;
+  tags: string[];
+};
+
 const Search: NextPage = () => {
   return (
     <>
