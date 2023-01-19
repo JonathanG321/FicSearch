84363fc504b5a53a7f47ffcd37bdcfc57830e60f
JonathanG321 <JonathanG321@ymail.com>
2023-01-18 23:48:07 -0500

    add searchForm component

---
+++ src/components/searchForm.tsx
@@ -0,0 +1,7 @@
+export type SearchFormType = {};
+
+function SearchForm({}: SearchFormType) {
+  return <div className="m-4 rounded-md border p-4 text-white"></div>;
+}
+
+export default SearchForm;
