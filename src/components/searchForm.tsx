ed544a9bb6a30795b12ce237952ce7c926406e69
JonathanG321 <JonathanG321@ymail.com>
2023-01-19 23:52:29 -0500

    add state and basic types to search component

--- src/components/searchForm.tsx
+++ src/components/searchForm.tsx
@@ -1,7 +1,16 @@
-export type SearchFormType = {};
+import Input from "./common/input";
+import { type AO3Search, type SpaceBattlesSearch } from "../types/search";
+import { useState } from "react";
+
+export type SearchFormType = { test: string };
 
 function SearchForm({}: SearchFormType) {
-  return <div className="m-4 rounded-md border p-4 text-white"></div>;
+  const [] = useState<AO3Search | SpaceBattlesSearch>({ anyField: "" });
+  return (
+    <div>
+      <Input value="test" onChange={() => undefined} />
+    </div>
+  );
 }
 
 export default SearchForm;
