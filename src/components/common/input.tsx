af87e98ac5b667918b0a632e36123db103688829
JonathanG321 <JonathanG321@ymail.com>
2023-01-20 22:16:16 -0500

    add label and change handler to Input component

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -1,7 +1,14 @@
-export type InputType = { value: string; onChange: () => void };
+import { type ChangeEventHandler } from "react";
 
-function Input({ value, onChange }: InputType) {
-  return <input onChange={onChange} className="rounded border px-2 py-1" value={value} />;
+export type InputType = { value: string; onChange: ChangeEventHandler<HTMLInputElement>; fieldName: string };
+
+function Input({ value, onChange, fieldName }: InputType) {
+  return (
+    <div>
+      <label htmlFor={fieldName}>{fieldName}</label>
+      <input name={fieldName} onChange={onChange} className="rounded border px-2 py-1" value={value} />
+    </div>
+  );
 }
 
 export default Input;
