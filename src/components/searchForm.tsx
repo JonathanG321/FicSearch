825d5bdbb17905824f8a79fde75516301753a48f
JonathanG321 <JonathanG321@ymail.com>
2023-01-20 22:23:12 -0500

    merge anyField and keywords form fields

--- src/components/searchForm.tsx
+++ src/components/searchForm.tsx
@@ -9,12 +9,15 @@ function SearchForm({}: SearchFormType) {
   const [SpaceBattlesData, setSpaceBattlesData] = useState<SpaceBattlesSearch>({});
   return (
     <div>
-      <h3>AO3 Search</h3>
-      <div className="rounded border p-4">
+      <h3>Universal</h3>
+      <div className="mb-4 rounded border p-4">
         <Input
           fieldName="anyField"
-          value="test"
-          onChange={(e) => setAO3Data({ ...AO3Data, anyField: e.currentTarget.value })}
+          value={AO3Data.anyField || SpaceBattlesData.keyWords}
+          onChange={(e) => {
+            setAO3Data({ ...AO3Data, anyField: e.currentTarget.value });
+            setSpaceBattlesData({ ...SpaceBattlesData, keyWords: e.currentTarget.value });
+          }}
         />
       </div>
     </div>
