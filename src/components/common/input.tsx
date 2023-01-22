ae97a30ded002321de5c524340ea8b63c3403ac0
JonathanG321 <JonathanG321@ymail.com>
2023-01-21 21:21:20 -0500

    fix input styling and add fieldText for better formatting

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -1,12 +1,26 @@
 import { type ChangeEventHandler } from "react";
+import { type AO3Search, type SpaceBattlesSearch } from "../../types/search";
 
-export type InputType = { value?: string; onChange: ChangeEventHandler<HTMLInputElement>; fieldName: string };
+export type InputType = {
+  value?: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  fieldName: keyof AO3Search | keyof SpaceBattlesSearch;
+  fieldText: string;
+};
 
-function Input({ value = "", onChange, fieldName }: InputType) {
+function Input({ value = "", onChange, fieldName, fieldText }: InputType) {
   return (
-    <div>
-      <label htmlFor={fieldName}>{fieldName}</label>
-      <input name={fieldName} onChange={onChange} className="rounded border px-2 py-1" value={value} />
+    <div className="flex flex-col">
+      <label className="hidden" htmlFor={fieldName}>
+        {fieldText}
+      </label>
+      <input
+        name={fieldName}
+        onChange={onChange}
+        className="rounded border px-2 py-1 text-black"
+        value={value}
+        placeholder={fieldText}
+      />
     </div>
   );
 }
