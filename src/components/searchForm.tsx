a5fd6949747985bd911da0aa4130cb712d302c2d
JonathanG321 <JonathanG321@ymail.com>
2023-01-21 21:21:38 -0500

    fix search form styling

--- src/components/searchForm.tsx
+++ src/components/searchForm.tsx
@@ -8,11 +8,12 @@ function SearchForm({}: SearchFormType) {
   const [AO3Data, setAO3Data] = useState<AO3Search>({});
   const [SpaceBattlesData, setSpaceBattlesData] = useState<SpaceBattlesSearch>({});
   return (
-    <div>
-      <h3>Universal</h3>
+    <div className="text-white">
+      <h2 className="mb-2 text-2xl">Universal</h2>
       <div className="mb-4 rounded border p-4">
         <Input
-          fieldName="anyField"
+          fieldName="keyWords"
+          fieldText="Key Words"
           value={AO3Data.anyField || SpaceBattlesData.keyWords}
           onChange={(e) => {
             setAO3Data({ ...AO3Data, anyField: e.currentTarget.value });
