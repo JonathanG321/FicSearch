e102003415e652159611fe4a47f19d53a76ae608
JonathanG321 <JonathanG321@ymail.com>
2023-01-22 23:29:09 -0500

    add select type to input component

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -3,24 +3,37 @@ import { type AO3Search, type SpaceBattlesSearch } from "../../types/search";
 
 export type InputType = {
   value?: string;
-  onChange: ChangeEventHandler<HTMLInputElement>;
+  onChange: ChangeEventHandler<HTMLInputElement | HTMLSelectElement>;
   fieldName: keyof AO3Search | keyof SpaceBattlesSearch;
   fieldText: string;
+  type?: string;
 };
 
-function Input({ value = "", onChange, fieldName, fieldText }: InputType) {
+function Input({ value = "", onChange, fieldName, fieldText, type = "input" }: InputType) {
   return (
     <div className="flex flex-col">
       <label className="hidden" htmlFor={fieldName}>
         {fieldText}
       </label>
-      <input
-        name={fieldName}
-        onChange={onChange}
-        className="rounded border px-2 py-1 text-black"
-        value={value}
-        placeholder={fieldText}
-      />
+      {type === "input" && (
+        <input
+          name={fieldName}
+          onChange={onChange}
+          className="rounded border px-2 py-1 text-black"
+          value={value}
+          placeholder={fieldText}
+        />
+      )}
+      {type === "select" && (
+        <select
+          name={fieldName}
+          onChange={onChange}
+          className="rounded border px-2 py-1 text-black"
+          multiple
+          value={value}
+          placeholder={fieldText}
+        />
+      )}
     </div>
   );
 }
