2282c9fdf8ac74a4acdb368cec1716a7fba0e6ca
JonathanG321 <JonathanG321@ymail.com>
2023-01-23 23:42:30 -0500

    fix search page styling

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -11,10 +11,8 @@ const Search: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <div className="flex flex-col items-center gap-2">
-            <SearchForm test="" />
-          </div>
+        <div className="flex flex-col items-center justify-center">
+          <SearchForm test="" />
         </div>
       </main>
     </>
