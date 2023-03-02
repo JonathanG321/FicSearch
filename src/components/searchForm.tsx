c1c0ad8f06002aa71b4f4fcfe22708e1370e2439
JonathanG321 <JonathanG321@ymail.com>
2023-03-01 21:34:59 -0500

    add more form fields

--- src/components/searchForm.tsx
+++ src/components/searchForm.tsx
@@ -8,6 +8,7 @@ import NumberField from "./common/Fields/NumberField";
 import { languages } from "../utils/languages";
 import ChipField from "./common/Fields/ChipField";
 import DropdownField from "./common/Fields/DropdownField";
+import CalendarField from "./common/Fields/CalendarField";
 
 export type SearchFormType = { test: string };
 
@@ -41,20 +42,22 @@ function SearchForm({}: SearchFormType) {
     getFieldMeta,
   };
   return (
-    <div className="text-white">
+    <div className="py-10 text-white">
       <form onSubmit={handleSubmit}>
         <h2 className="mb-2 text-2xl">Universal</h2>
         <Card className="w-full bg-gradient-to-b from-[#2e026d] to-[#15162c]">
           <TextField<SearchSchema> {...commonFormFunctions} label="Key Words" fieldPath="keyWords" />
           <ChipField<SearchSchema> {...commonFormFunctions} label="Tags" fieldPath="tags" />
+          <CalendarField<SearchSchema> {...commonFormFunctions} label="Older Than" fieldPath="olderThan" />
+          <CalendarField<SearchSchema> {...commonFormFunctions} label="Newer Than" fieldPath="newerThan" />
         </Card>
-        <h2 className="mb-2 text-2xl">Space Battles</h2>
+        <h2 className="my-2 text-2xl">Space Battles</h2>
         <Card className="w-full bg-gradient-to-b from-[#2e026d] to-[#15162c]">
           <ChipField<SearchSchema> {...commonFormFunctions} label="Exclude Tags" fieldPath="excludeTags" />
           <NumberField<SearchSchema> {...commonFormFunctions} label="Replies" fieldPath="replies" />
           <ChipField<SearchSchema> {...commonFormFunctions} label="Users" fieldPath="users" />
         </Card>
-        <h2 className="mb-2 text-2xl">Archive of Our Own</h2>
+        <h2 className="my-2 text-2xl">Archive of Our Own</h2>
         <Card className="w-full bg-gradient-to-b from-[#2e026d] to-[#15162c]">
           <DropdownField<SearchSchema>
             {...commonFormFunctions}
