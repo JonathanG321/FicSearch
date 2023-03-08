2e3eb24776116b35269880e00135fd0de1de273d
JonathanG321 <JonathanG321@ymail.com>
2023-03-07 23:38:09 -0500

    remove test

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -21,7 +21,7 @@ const Search: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <SearchProvider search={localSearch} updateSearch={setLocalSearch}>
-          <SearchForm test="" />
+          <SearchForm />
         </SearchProvider>
       </main>
     </>
